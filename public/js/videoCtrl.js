5b3bcd2d45507c2059d4c958c9afd3f14e86740e
yanoooooo <miserablescaromioben@gmail.com>
2016-08-30 15:38:56 +0900

    手の認識

--- public/js/videoCtrl.js
+++ public/js/videoCtrl.js
@@ -34,9 +34,110 @@ angular.module("hairselfy").controller('videoCtrl', function videoCtrl() {
     });
   }
 
+  //hand tracking
+  vm.HandTraking = function(){
+    var hand_video = $("#spVideo video")[0];
+    var that = vm, image, candidate;
+    
+    requestAnimationFrame( function() { return that.HandTraking(); } );
+    
+    console.dir(hand_video);
+    if (hand_video.readyState === hand_video.HAVE_ENOUGH_DATA){
+      image = snapshot(hand_video);
+      
+      candidate = vm.tracker.detect(image);
+      
+      draw(candidate);
+    }
+  };
+
+  function snapshot(video){
+    vm.context.drawImage(video, 0, 0, vm.canvas.width, vm.canvas.height);
+      
+    return vm.context.getImageData(0, 0, vm.canvas.width, vm.canvas.height);
+  }
+
+  function draw(candidate){
+    if (candidate){
+      drawHull(candidate.hull, "red");
+      /*if (this.cbxHull.checked){
+        drawHull(candidate.hull, "red");
+      }
+      
+      if (this.cbxDefects.checked){
+        this.drawDefects(candidate.defects, "blue");
+      }
+    }
+    
+    if (this.cbxSkin.checked){
+      this.context.putImageData(
+        this.createImage(this.tracker.mask, this.image), 
+        this.canvas.width - this.image.width,
+        this.canvas.height - this.image.height);
+    }*/
+    }
+  }
+  
+  function drawHull(hull, color){
+    var len = hull.length, i = 1;
+  
+    if (len > 0){
+      vm.context.beginPath();
+      vm.context.lineWidth = 3;
+      vm.context.strokeStyle = color;
+
+      vm.context.moveTo(hull[0].x, hull[0].y);
+      for (; i < len; ++ i){
+        vm.context.lineTo(hull[i].x, hull[i].y);
+      }
+
+      vm.context.stroke();
+      vm.context.closePath();
+    }
+  }
+  
+  /*DEMO.prototype.drawDefects = function(defects, color){
+    var len = defects.length, i = 0, point;
+  
+    if (len > 0){
+      this.context.beginPath();
+      this.context.lineWidth = 3;
+      this.context.strokeStyle = color;
+
+      for (; i < len; ++ i){
+        point = defects[i].depthPoint;
+        this.context.strokeRect(point.x - 4, point.y - 4, 8, 8);
+      }
+
+      this.context.stroke();
+      this.context.closePath();
+    }
+  };
+
+  DEMO.prototype.createImage = function(imageSrc, imageDst){
+    var src = imageSrc.data, dst = imageDst.data,
+        width = imageSrc.width, span = 4 * width,
+        len = src.length, i = 0, j = 0, k = 0;
+    
+    for(i = 0; i < len; i += span){
+    
+      for(j = 0; j < width; j += 5){
+      
+        dst[k] = dst[k + 1] = dst[k + 2] = src[i];
+        dst[k + 3] = 255;
+        k += 4;
+        
+        i += 5;
+      }
+    }
+    
+    return imageDst;
+  };*/
+
   vm.init = function(ua) {
     if (ua.indexOf('Android') > 0) {
       vm.user_agent = "sp";
+      vm.tracker = new HT.Tracker();
     }
 
     peer.on('open', function(){
@@ -56,7 +157,15 @@ angular.module("hairselfy").controller('videoCtrl', function videoCtrl() {
     getVideo(function(stream){
       window.localStream = stream;
       onReceiveStream(stream, vm.user_agent+'Video');
+      if(vm.user_agent == "sp") {
+        vm.HandTraking();
+      }
     });
+  };
+
+  vm.setCanvas = function() {
+    vm.canvas = document.getElementById("hand_tracking");
+    vm.context = vm.canvas.getContext("2d");
   };
 
   vm.login = function() {
