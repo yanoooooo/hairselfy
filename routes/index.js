70dc4427ed251218e2c74c4fb51550aad1f24088
yanoooooo <miserablescaromioben@gmail.com>
2016-09-05 18:06:52 +0900

    ベタ書きでindex作成

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Hair Selfy' });
 });
 
 router.get('/video', function(req, res, next) {
